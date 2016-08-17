c334d0f3b888e0b1c32a41588ddeb61538991165
JoramWilander <jwawilander@gmail.com>
2016-08-17 12:25:47 -0400

    Add stacking and clearing notification functionality

--- app/src/main/java/com/mattermost/gcm/GcmMessageHandler.java
+++ app/src/main/java/com/mattermost/gcm/GcmMessageHandler.java
@@ -16,21 +16,32 @@ import android.util.Log;
 import com.google.android.gms.gcm.GcmListenerService;
 import com.mattermost.mattermost.R;
 import com.mattermost.mattermost.SplashScreenActivity;
+import java.util.LinkedHashMap;
 
 public class GcmMessageHandler extends GcmListenerService {
 
     public static final int MESSAGE_NOTIFICATION_ID = 435345;
+    public static final String GROUP_KEY_MESSAGES = "group_key_messages";
+    private static LinkedHashMap<String,Bundle> channelIdToNotification = new LinkedHashMap<String,Bundle>();
+
+    public static void clearNotifications() {
+        channelIdToNotification.clear();
+    }
 
     @Override
     public void onMessageReceived(String from, Bundle data) {
-        createNotification("Mattermost", data.getString("message"));
+        String type = data.getString("type");
+        if ("clear".equals(type)) {
+            cancelNotification(data.getString("channel_id"));
+        } else {
+            channelIdToNotification.put(data.getString("channel_id"), data);
+            createNotification();
+        }
         super.onMessageReceived(from, data);
     }
 
     @Override
-    public void onDeletedMessages() {
-        super.onDeletedMessages();
-    }
+    public void onDeletedMessages() { super.onDeletedMessages(); }
 
     @Override
     public void onMessageSent(String msgId) {
@@ -42,7 +53,7 @@ public class GcmMessageHandler extends GcmListenerService {
         super.onSendError(msgId, error);
     }
 
-    private void createNotification(String title, String body) {
+    private void createNotification() {
         Context context = getBaseContext();
 
         int defaults = 0;
@@ -55,18 +66,55 @@ public class GcmMessageHandler extends GcmListenerService {
         notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
         PendingIntent contentIndent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
 
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle(title)
-                .setContentText(body)
-                .setDefaults(defaults)
-                .setStyle(new NotificationCompat.BigTextStyle()
-                        .bigText(body))
-                .setContentIntent(contentIndent)
-                .setAutoCancel(true);
+        PendingIntent deleteIntent = PendingIntent.getBroadcast(context, 0, new Intent(context, NotificationDismissReceiver.class), 0);
+
         NotificationManager mNotificationManager = (NotificationManager) context
                 .getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (channelIdToNotification.size() == 0) {
+            mNotificationManager.cancel(MESSAGE_NOTIFICATION_ID);
+            return;
+        }
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setGroup(GROUP_KEY_MESSAGES)
+                .setDefaults(defaults)
+                .setContentIntent(contentIndent)
+                .setDeleteIntent(deleteIntent)
+                .setAutoCancel(true);
+
+        if (channelIdToNotification.size() == 1) {
+            Bundle data = channelIdToNotification.entrySet().iterator().next().getValue();
+            String body = data.getString("message");
+            mBuilder.setContentTitle("Mattermost")
+                    .setContentText(body)
+                    .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(body));
+        } else {
+            NotificationCompat.InboxStyle style = new NotificationCompat.InboxStyle();
+
+            String summaryTitle = String.format("Mattermost (%d)", channelIdToNotification.size());
+            mBuilder.setContentTitle(summaryTitle);
+
+            for (Bundle data : channelIdToNotification.values()) {
+                style.addLine(data.getString("message"));
+            }
+
+            style.setBigContentTitle(summaryTitle);
+            mBuilder.setStyle(style);
+        }
+
+        mNotificationManager.cancel(MESSAGE_NOTIFICATION_ID);
         mNotificationManager.notify(MESSAGE_NOTIFICATION_ID, mBuilder.build());
     }
 
+    private void cancelNotification(String channelId) {
+        if (!channelIdToNotification.containsKey(channelId)) {
+            return;
+        }
+
+        channelIdToNotification.remove(channelId);
+        createNotification();
+    }
 }
